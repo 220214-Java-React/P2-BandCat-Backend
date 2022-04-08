c2c046a4f7769481d643f595a40032312faa8e20
Marcus Glover <marcusg73@yahoo.com>
2022-04-08 18:34:13 -0400

    Added class-level annotations for repositories

--- BandCat/src/main/java/com/bandcat/BandCat/repo/InstrumentRepo.java
+++ BandCat/src/main/java/com/bandcat/BandCat/repo/InstrumentRepo.java
@@ -1,5 +1,10 @@
 package com.bandcat.BandCat.repo;
 
-public class InstrumentRepo
+import com.bandcat.BandCat.model.Instrument;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstrumentRepo extends JpaRepository<Instrument, Integer>
 {
 }
--- BandCat/src/main/java/com/bandcat/BandCat/repo/UserRepo.java
+++ BandCat/src/main/java/com/bandcat/BandCat/repo/UserRepo.java
@@ -1,5 +1,10 @@
 package com.bandcat.BandCat.repo;
 
-public class UserRepo
+import com.bandcat.BandCat.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>
 {
 }

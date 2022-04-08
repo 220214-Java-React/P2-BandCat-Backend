b0fe9ef1d684b397be8cf70de354fc8ce9f88fec
Marcus Glover <marcusg73@yahoo.com>
2022-04-08 18:37:41 -0400

    Added class-level annotations for services

--- BandCat/src/main/java/com/bandcat/BandCat/service/InstrumentService.java
+++ BandCat/src/main/java/com/bandcat/BandCat/service/InstrumentService.java
@@ -1,5 +1,15 @@
 package com.bandcat.BandCat.service;
 
+import com.bandcat.BandCat.repo.InstrumentRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class InstrumentService
 {
+    private InstrumentRepo instrumentRepo;
+
+    public InstrumentService(InstrumentRepo iR)
+    {
+        this.instrumentRepo = iR;
+    }
 }
--- BandCat/src/main/java/com/bandcat/BandCat/service/UserService.java
+++ BandCat/src/main/java/com/bandcat/BandCat/service/UserService.java
@@ -1,5 +1,15 @@
 package com.bandcat.BandCat.service;
 
+import com.bandcat.BandCat.repo.UserRepo;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService
 {
+    private UserRepo userRepository;
+
+    public UserService(UserRepo uR)
+    {
+        this.userRepository = uR;
+    }
 }

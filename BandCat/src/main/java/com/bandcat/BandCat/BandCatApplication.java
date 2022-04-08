5723bb872fa30ffddb0d8e1723d6c598fa499abd
Marcus Glover <marcusg73@yahoo.com>
2022-04-08 10:31:27 -0400

    Removed readme from dev branch, init commit with base project

---
+++ BandCat/src/main/java/com/bandcat/BandCat/BandCatApplication.java
@@ -0,0 +1,13 @@
+package com.bandcat.BandCat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandCatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandCatApplication.class, args);
+	}
+
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-# P2-BandCat-Backend

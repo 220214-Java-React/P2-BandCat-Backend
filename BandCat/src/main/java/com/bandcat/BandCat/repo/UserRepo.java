17f64ed0818dba1c37df8dc583fcef703ffbe66c
Marcus Glover <marcusg73@yahoo.com>
2022-04-09 21:34:34 -0400

    Added method to find Users by their username

--- BandCat/src/main/java/com/bandcat/BandCat/repo/UserRepo.java
+++ BandCat/src/main/java/com/bandcat/BandCat/repo/UserRepo.java
@@ -4,6 +4,8 @@ import com.bandcat.BandCat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * This class is designed to handle the persistence logic
  * between Services and Database, for Users.
@@ -11,4 +13,13 @@ import org.springframework.stereotype.Repository;
  * to persist data to a database.
  */
 @Repository
-public interface UserRepo extends JpaRepository<User, Integer> {}
+public interface UserRepo extends JpaRepository<User, Integer>
+{
+    /**
+     * @author Elaine, Jazib, Marcus
+     * Method to find a User based on its username
+     * @param username The username to search for
+     * @return The User found
+     */
+    User findByUsername(String username);
+}

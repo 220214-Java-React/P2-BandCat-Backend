36519d5cd7708a4edef53b44d976a3dc095c27b6
Marcus Glover <marcusg73@yahoo.com>
2022-04-09 22:44:24 -0400

    Added test for new Instrument persistence through InstrumentService

--- BandCat/src/test/java/com/bandcat/BandCat/service/InstrumentServiceTests.java
+++ BandCat/src/test/java/com/bandcat/BandCat/service/InstrumentServiceTests.java
@@ -1,11 +1,15 @@
 package com.bandcat.BandCat.service;
 
+import com.bandcat.BandCat.model.Instrument;
+import com.bandcat.BandCat.model.InstrumentOptions;
+import com.bandcat.BandCat.model.User;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * This class is designed to create Tests for Instrument services.
@@ -26,6 +30,9 @@ public class InstrumentServiceTests
     @Autowired
     InstrumentService instrumentService;    // Service to use
 
+    @Autowired
+    UserService userService;
+
     /**
      * @author Marcus
      */
@@ -35,6 +42,21 @@ public class InstrumentServiceTests
         assertTrue(true);
     }
 
+    /**
+     * @author Marcus
+     */
+    @Test
+    @DisplayName("Test to persist new Instruments")
+    public void persistANewInstrumentWithTheInstrumentServiceClass()
+    {
+        User u = new User();            // Create a User to be assigned this instrument
+        userService.createNewUser(u);   // Persist them
 
+        // Create an Instrument
+        Instrument i = new Instrument(0, InstrumentOptions.CLARINET, 10, u);
+
+        // Check if the expected value matches the Instrument created
+        assertEquals(InstrumentOptions.CLARINET , instrumentService.createNewInstrument(i).getInstrumentName());
+    }
 
 }

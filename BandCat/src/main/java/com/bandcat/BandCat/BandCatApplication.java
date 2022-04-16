c847eeb76ba7b48604f9bbc6f814e247d8c613d1
Marcus Glover <marcusg73@yahoo.com>
2022-04-15 22:03:26 -0400

    Converted logger into a bean

--- BandCat/src/main/java/com/bandcat/BandCat/BandCatApplication.java
+++ BandCat/src/main/java/com/bandcat/BandCat/BandCatApplication.java
@@ -1,13 +1,22 @@
 package com.bandcat.BandCat;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BandCatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BandCatApplication.class, args);
+	}
+
+	@Bean
+	public Logger getTheLogger()
+	{
+		return LogManager.getLogger();
 	}
 
 }
